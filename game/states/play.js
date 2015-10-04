b6a5817c042ae1a79e902300fa830d974ebaeca5
austinprete <austinprete23@gmail.com>
2015-10-03 17:43:31 -0700

    Rename sprite to player

--- game/states/play.js
+++ game/states/play.js
@@ -4,22 +4,22 @@
   Play.prototype = {
     create: function() {
       this.game.physics.startSystem(Phaser.Physics.ARCADE);
-      this.sprite = this.game.add.sprite(this.game.world.centerX, 530, 'ship');
-      this.sprite.anchor.setTo(0.5, 0.5);
-      this.sprite.inputEnabled = true;
+      this.player = this.game.add.sprite(this.game.world.centerX, 530, 'ship');
+      this.player.anchor.setTo(0.5, 0.5);
+      this.player.inputEnabled = true;
 
-      this.game.physics.arcade.enable(this.sprite);
-      this.sprite.body.collideWorldBounds = true;
+      this.game.physics.arcade.enable(this.player);
+      this.player.body.collideWorldBounds = true;
 
 
     },
     update: function() {
-      if (this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT) && (this.sprite.body.position.x > 60))
+      if (this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT) && (this.player.body.position.x > 60))
       {
-        this.sprite.x -= 5;
-      } else if (this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT) && (this.sprite.body.position.x < 650))
+        this.player.x -= 5;
+      } else if (this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT) && (this.player.body.position.x < 650))
       {
-        this.sprite.x += 5;
+        this.player.x += 5;
       }
     },
     clickListener: function() {
